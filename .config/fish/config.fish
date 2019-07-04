10fa94e2f3f70010b2b27dcf38c86da6cde6ef98
gesi <helge.silset@nrk.no>
2019-07-04 12:06:48 +0200

    what?

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,25 @@
+# Silence greeting'
+set fish_greeting ""
+
 set -gx PATH \
   $PATH \
   $HOME/bin
+
+if test (uname) = "Darwin"
+  source ~/.config/fish/darwin.fish
+end
+
+set -l kube_configs ~/.kube/configs
+if test -d $kube_configs
+  echo (string join ':' (ls -d $kube_configs/*)) | read -a -x KUBECONFIG
+end
+
+set -l utils ~/.config/fish/utils
+if test -d $utils
+  for f in $utils/*.fish
+    source $f
+  end
+end
+
+nvm
 
