e22ded6d02f83d149db04a113ab165ff375a156d
gesi <helgesilset@gmail.com>
2023-05-23 18:03:26 +0200

    fix nix vscode extension function

--- fish/functions/vscode.fish
+++ fish/functions/vscode.fish
@@ -1,12 +1,12 @@
 function vscode_extension_to_nix_definition -a extension_id -a extension_version
-  set publisher (string split '.' $extension_id)[1]
-  set name (string split '.' $extension_id)[2]
-  set hash (nix-prefetch-url  "https://$publisher.gallery.vsassets.io/_apis/public/gallery/publisher/$publisher/extension/$name/$extension_version/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage" 2>/dev/null)
+    set publisher (string split '.' $extension_id)[1]
+    set name (string split '.' $extension_id)[2]
+    set hash (nix-prefetch-url  "https://$publisher.gallery.vsassets.io/_apis/public/gallery/publisher/$publisher/extension/$name/$extension_version/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage" 2>/dev/null)
 
-  echo "{
+    echo "{
   name = \"$name\";
   publisher = \"$publisher\";
-  version = \"$version\";
+  version = \"$extension_version\";
   sha256 = \"$hash\";
 }"
 end
