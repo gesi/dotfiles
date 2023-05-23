0172dbfddf081bfd1f9daf4a4811997a5bd63cca
gesi <helgesilset@gmail.com>
2023-05-23 18:06:05 +0200

    format fish config

--- modules/home/fish/config/config.fish
+++ modules/home/fish/config/config.fish
@@ -7,21 +7,21 @@ fish_vi_key_bindings
 set -gx PATH $HOME/dotfiles/bin $PATH
 
 # abbreviations
-abbr -a gco   git checkout
-abbr -a gc    git commit
-abbr -a gs    git status
-abbr -a gpl   git pull
-abbr -a gp    git push
-abbr -a gst   git stash
+abbr -a gco git checkout
+abbr -a gc git commit
+abbr -a gs git status
+abbr -a gpl git pull
+abbr -a gp git push
+abbr -a gst git stash
 
-abbr -a cat   bat
+abbr -a cat bat
 
-abbr -a ll    ls -l
-abbr -a lla   ls -la
+abbr -a ll ls -l
+abbr -a lla ls -la
 
-abbr -a k     kubectl
-abbr -a ktx   kubectx 
-abbr -a kns   kubens
+abbr -a k kubectl
+abbr -a ktx kubectx
+abbr -a kns kubens
 
 # aliases
 alias wpw "op read op://personal/jobb/password --no-newline | pbcopy"
@@ -51,6 +51,6 @@ set -gx DOCKER_HOST "unix:///$HOME/.local/share/containers/podman/machine/podman
 # pg
 fish_add_path /opt/homebrew/opt/postgresql@15/bin
 
-for f in $HOME/dotfiles/fish/**/*.fish;
-  source $f
+for f in $HOME/dotfiles/fish/**/*.fish
+    source $f
 end
